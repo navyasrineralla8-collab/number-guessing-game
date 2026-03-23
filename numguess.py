a0fc24c84171f5da31c6f6f4c3d0554d5b9c0ee7
navyasrineralla8-collab <navyasrineralla8@gmail.com>
2026-03-23 19:03:58 +0530

    Add files via upload
    Added number guessing game in Python with random module

---
+++ numguess.py
@@ -0,0 +1,19 @@
+import random
+def numguess():
+    print("🎮 welcome to number gussing game")
+    number=random.randint(1,100)
+    attempts=0
+    while True:
+        try:
+            guess=int(input("enter a number (1-100):"))
+            attempts+=1
+            if guess<number:
+                print("its lower tahan the number 📉 enter greater number")
+            elif guess>number:
+                print("it's greater then the number 📈 enter smaller number")
+            else:
+                print(f"🎉 correct! you guessed in {attempts} attempts.")
+                break
+        except:
+            print("please enter a valid number!")
+numguess()
